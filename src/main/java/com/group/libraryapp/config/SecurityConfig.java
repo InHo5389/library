fd96643455981657cda760a446cd7a3781f7f38f
JEONG <qheogus55@naver.com>
2024-05-11 04:07:39 +0900

    github action test

---
+++ src/main/java/com/group/libraryapp/config/SecurityConfig.java
@@ -0,0 +1,45 @@
+package com.group.libraryapp.config;
+
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfig {
+
+    @Bean
+    SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+        AuthenticationManagerBuilder sharedObject = http.getSharedObject(AuthenticationManagerBuilder.class);
+        AuthenticationManager authenticationManager = sharedObject.build();
+        http.authenticationManager(authenticationManager);
+
+
+        // 보안 설정 부분
+        http
+                .csrf(cs -> cs.disable())
+                .sessionManagement(sm -> sm.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .formLogin(fl -> fl.disable())
+                .httpBasic(hb -> hb.disable());
+
+        // 권한 설정 부분
+        http
+                .authorizeHttpRequests(authorize ->
+                        authorize
+                                .requestMatchers("/api/members/**").permitAll()
+                                .requestMatchers("/health-check").permitAll()
+                                .requestMatchers("/env").permitAll()
+                                .anyRequest().authenticated());
+        return http.build();
+    }
+}
